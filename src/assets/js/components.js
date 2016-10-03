d017b1e23164ce6418d5b64babc00af06d1f3a33
erifranck <erifranjose@hotmail.com>
2016-10-02 20:59:35 -0400

    custom sidebar

--- src/assets/js/components.js
+++ src/assets/js/components.js
@@ -1,10 +1,10 @@
 $('#right-menu').on('click', function() {
   $('.side-layout').addClass('active');
-  $('.side-container').addClass('slideInLeft');
-  $('.side-container').removeClass('slideOutLeft');
+  $('.side-container').addClass('bounceInLeft');
+  $('.side-container').removeClass('bounceOutLeft');
 });
 $('.side-layout').on('click', function() {
   setTimeout(() => $('.side-layout').removeClass('active'), 800);
-  $('.side-container').removeClass('slideInLeft');
-  $('.side-container').addClass('slideOutLeft');
+  $('.side-container').removeClass('bounceInLeft');
+  $('.side-container').addClass('bounceOutLeft');
 });
