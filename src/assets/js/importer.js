5d65fbdcfbc232a3863ea22e11726e2572915af4
erifranck <erifranjose@hotmail.com>
2016-10-03 02:03:55 -0400

    programing;

--- src/assets/js/importer.js
+++ src/assets/js/importer.js
@@ -1,1 +1,12 @@
+import { WOW } from 'wowjs';
 require('bootstrap');
+
+const wow = new WOW({
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       20,          // default
+  mobile:       true,       // default
+  live:         true        // default
+});
+console.log(wow);
+wow.init();
